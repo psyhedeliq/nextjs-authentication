189b38c4883a88abe3ebe56799cda78f844a0ef9
Cornel Filip <filipcornelcristian@gmail.com>
2024-06-04 06:20:55 +0300

    feat: add GitHub provider

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,26 @@
+import { NextPageContext } from 'next';
+import { useSession, signIn, signOut, getSession } from 'next-auth/react';
+
 export default function Home() {
+    const { data: session } = useSession();
+    console.log(session);
+
     return (
         <>
             <h1>Happy coding</h1>
+            <button onClick={() => signIn()}>Sign in</button> <br />
+            <button onClick={() => signOut()}>Sign out</button>
         </>
     );
+}
+
+export async function getServerSideProps(ctx: NextPageContext) {
+    const session = await getSession(ctx);
+    console.log(session);
+
+    return {
+        props: {
+            session,
+        },
+    };
 }
