e761c0d8490c4f96194d02813b4b24429b55c7dc
Cornel Filip <filipcornelcristian@gmail.com>
2024-06-12 16:43:12 +0300

    chore: add duplicate console log for testing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import { useSession, signIn, signOut, getSession } from 'next-auth/react';
 export default function Home() {
     const { data: session } = useSession();
     console.log('-- Session --');
+    console.log('-- Session --');
     console.log(session);
 
     return (
