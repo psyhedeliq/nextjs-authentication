762e7e2b8c28010f5b7dd86a1cd02ec9acbc3370
Cornel Filip <filipcornelcristian@gmail.com>
2024-06-17 18:26:22 +0300

    feat: Update build description with continuous integration and deployment details

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
     console.log('-- Session --');
     console.log(session);
     const text1 = `This is a simple example of how to use NextAuth.js with Next.js. It includes a sign-in form with support for signing in with Google, Facebook, GitHub, Twitter, Discord and Auth0.`;
-    const text2 = `In this build we use React.js, Next.js, Mongodb, Mongoose, Tailwind CSS, NextAuth.js, React Hook Form, Zod, Axios, Nodemailer, SMTP Service, Gmail SMTP, React Toastify, Handlebars and Bcrypt.`;
+    const text2 = `In this build we use React.js, Next.js, Mongodb, Mongoose, Tailwind CSS, NextAuth.js, React Hook Form, Zod, Axios, Nodemailer, SMTP Service, Gmail SMTP, React Toastify, Handlebars and Bcrypt. We have continuous integration and deployment with Vercel.`;
 
     return (
         <div className="home bg-black min-h-screen text-white flex items-center justify-center ">
