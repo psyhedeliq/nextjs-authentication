d53e7f7e4d90ef1807a0684e2d885832fb001602
Cornel Filip <filipcornelcristian@gmail.com>
2024-06-18 16:02:26 +0300

    feat: Add User model for storing user information

---
+++ src/models/User.ts
@@ -0,0 +1,41 @@
+import mongoose from 'mongoose';
+
+const defaultUserProfileImageUrl =
+    'https://res.cloudinary.com/dmhcnhtng/image/upload/v1664642479/992490_sskqn3.png';
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    image: {
+        type: String,
+        default: defaultUserProfileImageUrl,
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6,
+    },
+    emailVerified: {
+        type: Boolean,
+        default: false,
+    },
+    phone: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        default: 'user',
+    },
+});
+
+const User = mongoose.models.User || mongoose.model('User', userSchema);
+
+export default User;
