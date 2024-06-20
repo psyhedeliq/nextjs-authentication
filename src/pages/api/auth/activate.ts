5e7e6443c2a99f9295058a64f4fe3d1fe6f1fe4e
Cornel Filip <filipcornelcristian@gmail.com>
2024-06-20 13:52:38 +0300

    feat: Implement user account activation API endpoint

---
+++ src/pages/api/auth/activate.ts
@@ -0,0 +1,36 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introductionimport connectDb from '@/utils/connectDb';
+import User from '@/models/User';
+import jwt from 'jsonwebtoken';
+import connectDb from '@/utils/connectDb';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+const { ACTIVATION_TOKEN_SECRET } = process.env;
+interface UserToken {
+    id: string;
+}
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    try {
+        await connectDb();
+        const { token } = req.body;
+        const userToken = jwt.verify(
+            token,
+            ACTIVATION_TOKEN_SECRET!
+        ) as UserToken;
+
+        const userDb = await User.findById(userToken.id);
+        if (userDb.emailVerified) {
+            return res.status(400).json({ message: 'User already activated.' });
+        }
+        await User.findByIdAndUpdate(userDb.id, { emailVerified: true });
+
+        res.json({
+            message: 'Your account has been successfully verified.',
+        });
+    } catch (error) {
+        res.status(500).json({ message: (error as Error).message });
+    }
+}
