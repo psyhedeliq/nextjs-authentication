32c9586254689553eedec450fa58b6dacd349e9f
Cornel Filip <filipcornelcristian@gmail.com>
2024-06-21 13:25:40 +0300

    refactor: Update auth page to use dynamic image based on tab value

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -1,30 +1,35 @@
 import Background from '@/components/backgrounds/Background';
+import LoginForm from '@/components/forms/Login';
 import RegisterForm from '@/components/forms/Register';
+import { NextPageContext } from 'next';
 
-export default function auth() {
+export default function auth({ tab }: { tab: string }) {
+    console.log(tab);
+
     return (
         <div className="w-full flex items-center justify-center">
             <div className="w-full h-100 flex items-center justify-center">
                 {/* --Form-- */}
                 <div className="w-full sm:w-5/6 md:w-2/3 lg:w-1/2 xl:w-1/3 2xl:w-1/3 h-full bg-white flex items-center justify-center">
-                    <div className="w-full px-12 py-4">
-                        <h2 className="text-center text-2xl font-bold tracking-wide text-gray-800">
-                            Sign up
-                        </h2>
-                        <p className="text-center text-sm text-gray-600 mt-2">
-                            You already have an account? &nbsp;
-                            <a className="text-blue-600 hover:text-blue-700 hover:underline cursor-pointer">
-                                Sign in
-                            </a>
-                        </p>
-                        {/* SIGN UP FORM */}
-                        <RegisterForm />
-                    </div>
+                    {tab == 'signin' ? <LoginForm /> : <RegisterForm />}
                 </div>
 
                 {/* --Background-- */}
-                <Background image="/register.jpg" />
+                <Background
+                    image={`"/${tab == 'signup' ? 'register' : 'signin'}.jpg"`}
+                />
             </div>
         </div>
     );
 }
+
+export async function getServerSideProps(ctx: NextPageContext) {
+    const { req, query } = ctx;
+    const tab = query.tab ? query.tab : 'signin';
+
+    return {
+        props: { tab: JSON.parse(JSON.stringify(tab)) },
+    };
+}
+
+// TODO: Change picture if sign in
