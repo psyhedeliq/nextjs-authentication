b8b792740a59dee2dcd96790972ce11a049168bd
Cornel Filip <filipcornelcristian@gmail.com>
2024-06-21 15:32:28 +0300

    refactor: Update auth page to include social media authentication buttons

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -1,25 +1,26 @@
 import Background from '@/components/backgrounds/Background';
+import SocialButton from '@/components/buttons/SocialButton';
 import LoginForm from '@/components/forms/Login';
 import RegisterForm from '@/components/forms/Register';
 import { NextPageContext } from 'next';
-import { getCsrfToken } from 'next-auth/react';
+import { getCsrfToken, getProviders } from 'next-auth/react';
 
 export default function auth({
     tab,
     callbackUrl,
     csrfToken,
+    providers,
 }: {
     tab: string;
     callbackUrl: string;
     csrfToken: string;
+    providers: any;
 }) {
-    // console.log(tab);
-
     return (
         <div className="w-full flex items-center justify-center">
             <div className="w-full h-100 flex items-center justify-center">
                 {/* --Form-- */}
-                <div className="w-full sm:w-5/6 md:w-2/3 lg:w-1/2 xl:w-1/3 2xl:w-1/3 h-full bg-white flex items-center justify-center">
+                <div className="w-full sm:w-5/6 md:w-2/3 lg:w-1/2 xl:w-1/3 2xl:w-1/3 h-full bg-white flex flex-col items-center justify-center">
                     {tab == 'signin' ? (
                         <LoginForm
                             callbackUrl={callbackUrl}
@@ -28,6 +29,30 @@ export default function auth({
                     ) : (
                         <RegisterForm />
                     )}
+                    <div className="w-[90%] flex items-center justify-between">
+                        <div className="w-full h-[1px] bg-gray-300"></div>
+                        <span className="text-sm uppercase mx-6 text-gray-400">
+                            Or
+                        </span>
+                        <div className="w-full h-[1px] bg-gray-300"></div>
+                    </div>
+                    <div className="mt-3 grid grid-cols-2 gap-x-4 gat-y-2">
+                        {providers.map((provider: any) => {
+                            if (provider.name == 'Credentials') return;
+                            return (
+                                <SocialButton
+                                    key={provider.id}
+                                    id={provider.id}
+                                    text={
+                                        tab == 'signup'
+                                            ? `Sign up with ${provider.name}`
+                                            : `Sign in with ${provider.name}`
+                                    }
+                                    csrfToken={csrfToken}
+                                />
+                            );
+                        })}
+                    </div>
                 </div>
 
                 {/* --Background-- */}
@@ -45,10 +70,17 @@ export async function getServerSideProps(ctx: NextPageContext) {
     const callbackUrl = query.callbackUrl
         ? query.callbackUrl
         : process.env.NEXTAUTH_URL;
+
     const csrfToken = await getCsrfToken(ctx);
+    const providers = await getProviders();
 
     return {
-        props: { tab: JSON.parse(JSON.stringify(tab)), callbackUrl, csrfToken },
+        props: {
+            providers: providers ? Object.values(providers) : [],
+            tab: JSON.parse(JSON.stringify(tab)),
+            callbackUrl,
+            csrfToken,
+        },
     };
 }
 
